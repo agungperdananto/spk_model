1d3da7d7b5216ce1ebd50e4325bdfc2ec4123825
Agung  Perdananto <agung.perdananto@sirclo.com>
2023-11-14 09:41:36 +0700

    add sql data

---
+++ house_202311140940.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.house (developer,lb,lt,price) VALUES
+	 ('ciputra',75,80,850),
+	 ('sinarmas',70,50,950),
+	 ('sasmita',78,82,650),
+	 ('lippo',65,50,700),
+	 ('ciputra',65,70,750),
+	 ('sinarmas',120,110,1500);
